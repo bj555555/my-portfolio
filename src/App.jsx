3ca244e6c03ce6eb70d5569cb5c53e3c41279c24
bj555555 <bijaykalikoti8@gmail.com>
2026-05-11 19:52:05 +0100

    Update portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -146,7 +146,7 @@ function App() {
               <div className="c-icon">📱</div>
               <div><div className="c-label">Phone</div><div className="c-val">+447824075832</div></div>
             </div>
-            <a href="www.linkedin.com/in/bijay-kalikote-17b2982b2" target="_blank" rel="noreferrer" className="clink cl3">
+            <a href="https://www.linkedin.com/in/bijay-kalikote-17b2982b2/" target="_blank" rel="noreferrer" className="clink cl3">
               <div className="c-icon">💼</div>
               <div><div className="c-label">LinkedIn</div><div className="c-val">bijay-kalikoti</div></div>
             </a>
